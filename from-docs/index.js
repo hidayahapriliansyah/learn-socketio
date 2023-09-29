186d142af8fca9f81d352645ec6312f4b25cf16d
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-09-29 08:49:40 +0700

    save

--- from-docs/index.js
+++ from-docs/index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const { instrument } = require("@socket.io/admin-ui");
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  }
+});
+
+instrument(io, {
+  auth: false
+});
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
@@ -17,13 +27,20 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('chat message');
 
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', (msg, msg2) => {
+    console.log('msg =>>', msg);
+    console.log('msg2 =>>', msg2);
     io.emit('chat message', msg);
   });
+  // socket.on('chat message', (arg, callback) => {
+  //   console.log('arg =>>', arg);
+  //   io.emit('chat message', arg);
+  //   callback('terkirim')
+  // });
 
-  socket.on('show alert', (msg) => {
-    io.emit('client show alert', msg);
-  })
+  // socket.on('show alert', (msg) => {
+  //   io.emit('client show alert', msg);
+  // })
 });
 
 server.listen(3000, () => {
